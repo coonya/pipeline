33da75b6b6a92b7d53541a3c90535e12c6f38a21
duke <duke@duke-2.local>
2015-07-10 00:10:54 +0900

    second commit

--- test.py
+++ test.py
@@ -4,3 +4,9 @@ address = 'http://admin-apps.webofknowledge.com/JCR/JCR?RQ=LIST_SUMMARY_JOURNAL&
 website = urllib2.urlopen(address)
 website_html = website.read()
 print website_html
+
+
+
+
+
+print 'aaaaaaaaaaaaa'
